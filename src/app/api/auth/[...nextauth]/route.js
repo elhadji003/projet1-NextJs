02abc08cca68cc24fc97bf74d75ababd7654aa0c
Piod Gamer <malickelhadji07@gmail.com>
2024-05-27 16:18:07 +0000

    new commit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
-import NextAuth from 'next-auth';
-import { authOptions } from '../../../../../lib/auth-options'
+import NextAuth from "next-auth";
+import { authOptions } from "../authOptions";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handler = (req, res) => NextAuth(req, res, authOptions);
 
+export const GET = handler;
+export const POST = handler;
--- src/app/api/auth/authOptions.js
+++ src/app/api/auth/authOptions.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { connectMongoDB } from "../lib/mongodb";
-import User from "../models/user";
+import { connectMongoDB } from "../../../../lib/mongodb";
+import User from "../../../../models/user";
 import bcrypt from "bcryptjs";
 
 export const authOptions = {
