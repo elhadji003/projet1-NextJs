01d66e6320eb3c52af44b17f0728369bf9286b5e
Piod Gamer <malickelhadji07@gmail.com>
2024-05-28 09:08:10 +0000

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_HOSTNAME: "http://localhost:3000/api/",
+        MONGODB_URI:"mongodb+srv://malickelhadji07:bF36oYeHFzWHGBkP@cluster0.im422wh.mongodb.net/Cluster0",
+        NEXTAUTH_SECRET: "elhadjimalickdiop170503linguere",
+        NEXTAUTH_URL: "http://localhost:3000",
+        SECRET: "RONDOM_STRING",
+    }
+};
 
 
 export default nextConfig;
